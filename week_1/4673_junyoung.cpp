391c293c005b2546d0ec03984595e7da53a8eb9b
joju <whwndud92@gmail.com>
2017-06-30 14:24:14 +0900

    reset junyoung.cpp

--- week_1/4673_junyoung.cpp
+++ week_1/4673_junyoung.cpp
@@ -3,25 +3,4 @@ using namespace std;
 
 int main() {
 
-    bool array[10000] = {false};
-
-    int tmp;
-    int tmpPiece;
-    int sum;
-
-    for (int i = 1; i < 10000; ++i) {
-        sum = tmp = i;
-        while (tmp > 0) {
-            tmpPiece = tmp % 10;
-            tmp = tmp / 10;
-            sum += tmpPiece;
-        }
-        if (!array[sum])
-            array[sum] = true;
-    }
-    for (int i = 1; i < 10000; ++i) {
-        if (!array[i]) cout << i << endl;
-    }
-
-    return 0;
 }

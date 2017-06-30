88eae9fa23dc33ad1cb8a4e8d67005f8c5f49db2
joju <whwndud92@gmail.com>
2017-06-30 14:14:30 +0900

    [jujoju] 4673

--- week_1/1000_junyoung.cpp
+++ week_1/1000_junyoung.cpp
@@ -1,5 +1,4 @@
 // 이 파일은 예제파일입니다.
-
 #include <iostream>
 using namespace std;
 
--- week_1/4673_junyoung.cpp
+++ week_1/4673_junyoung.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
 
+    bool array[10000] = {false};
+
+    int tmp;
+    int tmpPiece;
+    int sum;
+
+    for (int i = 1; i < 10000; ++i) {
+        sum = tmp = i;
+        while (tmp > 0) {
+            tmpPiece = tmp % 10;
+            tmp = tmp / 10;
+            sum += tmpPiece;
+        }
+        if (!array[sum])
+            array[sum] = true;
+    }
+    for (int i = 1; i < 10000; ++i) {
+        if (!array[i]) cout << i << endl;
+    }
+
+    return 0;
 }

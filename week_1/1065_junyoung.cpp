6af65a819c64c4ad42756ca2bdf40925757d3a93
sungjunyoung <wnsdud1861@gmail.com>
2017-06-30 14:53:04 +0900

    [성준영] 1065번 완료

---
+++ week_1/1065_junyoung.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    // 개수 새는 카운터
+    int count = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int tempI = i;
+        // 각각 자리수를 저장
+        vector<int> numbers;
+        while (tempI > 0) {
+            numbers.push_back(tempI % 10);
+            tempI = tempI / 10;
+        }
+
+        bool check = true;
+        int sub = numbers[1]-numbers[0];
+        for (int j = 1; j < numbers.size(); j++) {
+            int tempSub = numbers[j] - numbers[j-1];
+            if(sub != tempSub){
+                check = false;
+                continue;
+            }
+        }
+
+        if(check){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+}

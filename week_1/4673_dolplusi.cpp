a884b276d8e50b20e3ef686565fe5cb489cb9afb
Min-ho Kim <minmin0416@naver.com>
2017-07-01 21:56:07 +0900

    [김민호]4673

---
+++ week_1/4673_dolplusi.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+bool checkNotSelfNumb[10001];
+
+int Dfunc(int num) {
+	int result = num;
+	int oneNum;
+	while (num != 0) {
+		oneNum = num % 10; num /= 10;
+		result += oneNum;
+	}
+	return result;
+}
+
+int main() {
+	int dump = 0;
+
+	for (int cnt = 1; dump <= 10036; cnt++) {
+		dump = Dfunc(cnt);
+		checkNotSelfNumb[dump] = true;
+	}
+	for (int cnt = 1; cnt < 10001; cnt++) {
+		if (checkNotSelfNumb[cnt] == false)
+			cout << cnt << endl;
+	}
+	return 0;
+}

da854d1b4874a0702ad5b108f681392cfc3af1ce
sungjunyoung <wnsdud1861@gmail.com>
2017-07-03 18:41:36 +0900

    [성준영] 1152번 조건문 추가하기보다 반복문 횟수를 줄이기

--- week_2/1152_junyoung.cpp
+++ week_2/1152_junyoung.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
 int main() {
@@ -11,15 +12,15 @@ int main() {
 
     int count = 1;
     // 띄워쓰기 개수로 단어 개수 파악. 첫번째, 마지막 띄워쓰기는 포함하지 않음
-    for (int i = 0; i < input.length(); i++) {
-        if (input[i] == ' ' && i != 0 && i != input.length() - 1) {
+    for (int i = 1; i < input.length() - 1; i++) {
+        if (input[i] == ' ') {
             count++;
         }
     }
 
     // 단어 개수가 하나로 나오는 경우, 하나의 띄워쓰기라면 count = 0, 오직 한 단어로 되어있다면 넘어감
     if (count == 1) {
-        if(input == " "){
+        if (input == " ") {
             count = 0;
         }
     }

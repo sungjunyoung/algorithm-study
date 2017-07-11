4e52facd7f4174655e50e0a727deaf6b3649f464
Min-ho Kim <minmin0416@naver.com>
2017-07-12 00:11:40 +0900

    [김민호] 10844 해결

---
+++ week_3/10844_dolplusi.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N, result = 0;
+	int D[101][10] = { 0, };
+	cin >> N;
+	for (int i = 1; i <= 9; i++) {
+		D[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j <= 9; j++) {
+			if (j == 9) {
+				D[i][j] = D[i - 1][j - 1] % 1000000000;
+				continue;
+			}
+			if (j == 0) {
+				D[i][j] = D[i - 1][j + 1] % 1000000000;
+				continue;
+			}
+			D[i][j] = (D[i - 1][j + 1] + D[i - 1][j - 1]) % 1000000000;
+		}
+	}
+
+	for (int i = 0; i <= 9; i++) {
+		result = (result + D[N][i]) % 1000000000;
+	}
+	cout << result % 1000000000;
+}
